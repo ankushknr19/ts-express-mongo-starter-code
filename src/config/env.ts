cfa0f521245b48e254895632c7af7fcc5b788b33
Ankush Kunwar <ank.knr@gmail.com>
2023-01-08 23:32:55 +0545

    added DEV_ENV as env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,9 +3,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const NODE_ENV = process.env.NODE_ENV as string
+export const DEV_ENV = process.env.DEV_ENV as string
 export const PORT = (process.env.PORT ?? 5800) as number
 
-const prod: boolean = NODE_ENV === 'production'
-export const MONGO_URI = prod
-	? (process.env.MONGO_PROD as string)
-	: (process.env.MONGO_LOCAL as string)
+export const MONGO_URI =
+	NODE_ENV === 'production'
+		? (process.env.MONGO_PROD as string)
+		: DEV_ENV === 'online'
+		? (process.env.MONGO_LOCAL_ONLINE as string)
+		: (process.env.MONGO_LOCAL_OFFLINE as string)
